a7f91670ea24369ed8d149865ff40fda8ebb784f
krillin-avi <cesdaille@knights.ucf.edu>
2023-03-28 17:28:23 -0400

    EMP & Navmesh Commit

--- gpPrototype/Assets/Scripts/Grenade.cs
+++ gpPrototype/Assets/Scripts/Grenade.cs
@@ -1,11 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Grenade : MonoBehaviour
 {
 
-    public float delay = 3f;
+    public float delay = 2f;
+    public float force = 800f;
+    public float radius = 20f;
+    public float empDowntime;
+
+    // Layer Mask for EMP Testing
+    public int layerMask = 1 << 9;
 
     public GameObject explosionEffect;
 
@@ -29,9 +36,35 @@ public class Grenade : MonoBehaviour
 
     void Explode()
     {
-        Debug.Log("EMP Goes Boom!");
         Instantiate(explosionEffect, transform.position, transform.rotation);
+
+        Collider[] colliders = Physics.OverlapSphere(transform.position, radius, layerMask);
+
+        foreach (Collider nearbyObject in colliders)
+        {
+            Rigidbody rb = nearbyObject.GetComponent<Rigidbody>();
+
+            // Collect AI Script to call on "Walk Point Set"
+            EnemyAI agentScript = nearbyObject.GetComponent<EnemyAI>();
+
+            if (rb != null)
+            {
+                rb.AddExplosionForce(force, transform.position, radius);
+                // Disable "Walk Point Set, wait timer, then enable walkpoint."
+                agentScript.walkPointSet = false;
+                StartCoroutine(EMPShutdown());
+                agentScript.walkPointSet = true;
+            }
+        }
 
         Destroy(gameObject);
     }
+
+    public IEnumerator EMPShutdown() 
+    {
+        yield return new WaitForSeconds(empDowntime);
+
+
+    }
+
 }
